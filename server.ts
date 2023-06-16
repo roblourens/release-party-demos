030d5d4d4466ab8f2d6150881783cc74ef4be738
Rob Lourens <roblourens@gmail.com>
2023-06-15 17:24:01 -0700

    update

--- server.ts
+++ server.ts
@@ -1,19 +1,12 @@
 import express from 'express';
 
-/**
- * Starts the server on the specified port.
- * @param port The port number to listen on.
- */
-function startServer(port: number) {
-    const app = express();
+const port = 3000;
+const app = express();
 
-    app.get('/', (req, res) => {
-        res.send('Hello World!');
-    });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-    app.listen(port, () => {
-        console.log(`Server listening at http://localhost:${port}`);
-    });
-}
-
-startServer(3000);
+app.listen(serverPort, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
